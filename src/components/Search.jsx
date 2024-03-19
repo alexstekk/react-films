597a634f0458fd851d2aecba5221a3ae6aaf5796
Alexander Stekolshchikov <alexstekk@yandex.ru>
2024-03-19 07:04:38 +0300

    Class components to functional components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,80 +1,153 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Search extends React.Component {
-	state = {
-		search: '',
-		type: '',
+// class Search extends React.Component {
+// 	state = {
+// 		search: '',
+// 		type: '',
+// 	};
+
+// 	handleRadio = (e) => {
+// 		this.setState({ type: e.target.value }, () => {
+// 			this.props.handleSearch(this.state.search, this.state.type);
+// 		});
+// 	};
+
+// 	render() {
+// 		const { handleSearch } = this.props;
+// 		return (
+// 			<div className='search'>
+// 				<input
+// 					type='search'
+// 					placeholder='Search'
+// 					className='search__input'
+// 					value={this.state.search}
+// 					onChange={(e) => this.setState({ search: e.target.value })}
+// 					onKeyDown={(e) => {
+// 						if (e.key === 'Enter') {
+// 							handleSearch(this.state.search, this.state.type);
+// 						}
+// 					}}
+// 				/>
+// 				<button
+// 					className='search__button'
+// 					onClick={() => {
+// 						if (this.state.search) {
+// 							handleSearch(this.state.search, this.state.type);
+// 						}
+// 					}}>
+// 					Search
+// 				</button>
+// 				<div className='search__type'>
+// 					<label>
+// 						<input
+// 							className='search__radio'
+// 							type='radio'
+// 							value=''
+// 							onChange={this.handleRadio}
+// 							checked={this.state.type === ''}
+// 							name='filter'
+// 						/>
+// 						All
+// 					</label>
+// 					<label>
+// 						<input
+// 							className='search__radio'
+// 							type='radio'
+// 							value='movie'
+// 							onChange={this.handleRadio}
+// 							checked={this.state.type === 'movie'}
+// 							name='filter'
+// 						/>
+// 						Movie
+// 					</label>
+// 					<label>
+// 						<input
+// 							className='search__radio'
+// 							type='radio'
+// 							value='series'
+// 							onChange={this.handleRadio}
+// 							checked={this.state.type === 'series'}
+// 							name='filter'
+// 						/>
+// 						Series
+// 					</label>
+// 				</div>
+// 			</div>
+// 		);
+// 	}
+// }
+
+function Search(props) {
+	const { handleSearch } = props;
+
+	const [search, setSearch] = useState('');
+	const [type, setType] = useState('');
+
+	const handleType = (e) => {
+		setType(e.target.value);
+		handleSearch(search, e.target.value);
 	};
 
-	handleRadio = (e) => {
-		this.setState({ type: e.target.value }, () => {
-			this.props.handleSearch(this.state.search, this.state.type);
-		});
-	};
-
-	render() {
-		const { handleSearch } = this.props;
-		return (
-			<div className='search'>
-				<input
-					type='search'
-					placeholder='Search'
-					className='search__input'
-					value={this.state.search}
-					onChange={(e) => this.setState({ search: e.target.value })}
-					onKeyDown={(e) => {
-						if (e.key === 'Enter') {
-							handleSearch(this.state.search, this.state.type);
-						}
-					}}
-				/>
-				<button
-					className='search__button'
-					onClick={() => {
-						if (this.state.search) {
-							handleSearch(this.state.search, this.state.type);
-						}
-					}}>
-					Search
-				</button>
-				<div className='search__type'>
-					<label>
-						<input
-							className='search__radio'
-							type='radio'
-							value=''
-							onChange={this.handleRadio}
-							checked={this.state.type === ''}
-							name='filter'
-						/>
-						All
-					</label>
-					<label>
-						<input
-							className='search__radio'
-							type='radio'
-							value='movie'
-							onChange={this.handleRadio}
-							checked={this.state.type === 'movie'}
-							name='filter'
-						/>
-						Movie
-					</label>
-					<label>
-						<input
-							className='search__radio'
-							type='radio'
-							value='series'
-							onChange={this.handleRadio}
-							checked={this.state.type === 'series'}
-							name='filter'
-						/>
-						Series
-					</label>
-				</div>
+	return (
+		<div className='search'>
+			<input
+				type='search'
+				placeholder='Search'
+				className='search__input'
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+				onKeyDown={(e) => {
+					if (e.key === 'Enter') {
+						handleSearch(search, type);
+					}
+				}}
+			/>
+			<button
+				className='search__button'
+				onClick={() => {
+					if (search) {
+						handleSearch(search, type);
+					}
+				}}>
+				Search
+			</button>
+			<div className='search__type'>
+				<label>
+					<input
+						className='search__radio'
+						type='radio'
+						value=''
+						onChange={handleType}
+						checked={type === ''}
+						name='filter'
+					/>
+					All
+				</label>
+				<label>
+					<input
+						className='search__radio'
+						type='radio'
+						value='movie'
+						onChange={handleType}
+						checked={type === 'movie'}
+						name='filter'
+					/>
+					Movie
+				</label>
+				<label>
+					<input
+						className='search__radio'
+						type='radio'
+						value='series'
+						onChange={handleType}
+						checked={type === 'series'}
+						name='filter'
+					/>
+					Series
+				</label>
 			</div>
-		);
-	}
+		</div>
+	);
 }
 
 export { Search };
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,42 +1,77 @@
 import { Movies } from '../components/Movies';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Preloader } from '../components/Preloader';
 import { Search } from '../components/Search';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-class Main extends React.Component {
-	state = {
-		movies: [],
-		isLoading: true,
-	};
-	componentDidMount() {
-		fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=kung-fu`)
-			.then((response) => response.json())
-			.then((data) => this.setState({ movies: data.Search, isLoading: false }));
-	}
+// class Main extends React.Component {
+// 	state = {
+// 		movies: [],
+// 		isLoading: true,
+// 	};
+// 	componentDidMount() {
+// 		fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=kung-fu`)
+// 			.then((response) => response.json())
+// 			.then((data) => this.setState({ movies: data.Search, isLoading: false }));
+// 	}
 
-	handleSearch = (query, type) => {
+// 	handleSearch = (query, type) => {
+// 		if (query) {
+// 			this.setState({ isLoading: true });
+// 			fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}${type === '' ? '' : `&type=${type}`}`)
+// 				.then((response) => response.json())
+// 				.then((data) => this.setState({ movies: data.Search, isLoading: false }));
+// 		} else return;
+// 	};
+
+// 	render() {
+// 		const { movies, isLoading } = this.state;
+
+// 		return (
+// 			<main className='main'>
+// 				<div className='main__container container'>
+// 					<Search handleSearch={this.handleSearch} />
+// 					{isLoading ? <Preloader /> : <Movies movies={movies} />}
+// 				</div>
+// 			</main>
+// 		);
+// 	}
+// }
+
+function Main() {
+	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	const handleSearch = (query, type) => {
 		if (query) {
-			this.setState({ isLoading: true });
+			setIsLoading(true);
 			fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}${type === '' ? '' : `&type=${type}`}`)
 				.then((response) => response.json())
-				.then((data) => this.setState({ movies: data.Search, isLoading: false }));
+				.then((data) => {
+					setMovies(data.Search);
+					setIsLoading(false);
+				});
 		} else return;
 	};
 
-	render() {
-		const { movies, isLoading } = this.state;
+	useEffect(() => {
+		fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=kung-fu`)
+			.then((response) => response.json())
+			.then((data) => {
+				setMovies(data.Search);
+				setIsLoading(false);
+			});
+	}, []);
 
-		return (
-			<main className='main'>
-				<div className='main__container container'>
-					<Search handleSearch={this.handleSearch} />
-					{isLoading ? <Preloader /> : <Movies movies={movies} />}
-				</div>
-			</main>
-		);
-	}
+	return (
+		<main className='main'>
+			<div className='main__container container'>
+				<Search handleSearch={handleSearch} />
+				{isLoading ? <Preloader /> : <Movies movies={movies} />}
+			</div>
+		</main>
+	);
 }
 
 export { Main };
